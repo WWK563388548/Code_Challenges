da692acd33270d6c282e82bd9da34b2f7f1501d0
WWK563388548 <wc563388548@gmail.com>
2018-08-08 17:57:35 +0900

    add solution of maxchar

--- 4_maxchar.js
+++ 4_maxchar.js
@@ -7,3 +7,31 @@
  * 
  * 语言： Javascript(ES6)
  */
+
+ function maxChar(str) {
+     const charMap = {};
+     let max = 0;
+     let maxChar = '';
+
+     // Map the chars and calculate their number
+     for(let char of str) {
+        if(charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+     }
+
+     console.log(charMap)
+     // Check which char is max char
+     for(let char in charMap) {
+         if(charMap[char] > max) {
+             max = charMap[char];
+             maxChar = char;
+         }
+     }
+
+     return maxChar
+ }
+
+ console.log(maxChar('Hello World!')); // => 'l'
