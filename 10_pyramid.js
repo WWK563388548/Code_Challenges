0f44ca2d86a28eb87a1dda48d9e5dd05b32cd0fd
WWK563388548 <wc563388548@gmail.com>
2018-11-22 09:12:26 +0900

    add steps questions

--- 10_pyramid.js
+++ 10_pyramid.js
@@ -16,3 +16,48 @@
  * 
  * 语言： Javascript(ES6)
  */
+
+ // method one
+ function pyramid1(n) {
+     const midpoint = Math.floor((2 * n - 1) / 2);
+     for(let row = 0; row < n; row++){
+         let level = '';
+
+         for(let column = 0; column < 2 * n - 1; column++){
+             if(midpoint - row <= column && midpoint + row >= column){
+                 level += '#';
+             } else {
+                 level += ' ';
+             }
+         }
+         console.log(level);
+     }
+ }
+
+ // method two(recursion solution)
+ function pyramid2(n, row = 0, level = '') {
+    if(row === n){
+        return;
+    }
+
+    if(level.length === 2 * n - 1){
+        console.log(level);
+        return pyramid2(n, row + 1);
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+    if(midpoint - row <= level.length && midpoint + row >= level.length){
+        add = '#';
+    } else {
+        add = ' ';
+    }
+
+    pyramid2(n, row, level + add);
+ }
+
+ pyramid1(6);
+ pyramid1(11);
+
+ pyramid2(5);
+ pyramid2(20);
--- 9_printing_steps.js
+++ 9_printing_steps.js
@@ -17,3 +17,57 @@
  * 
  * 语言： Javascript(ES6)
  */
+
+ // method one
+ function steps1(n){
+    for (let row = 0; row < n; row++){
+        let stair = '';
+
+        for(let column = 0; column < n; column++){
+            if(column <= row){
+                stair += "#";
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log(stair);
+    }
+ }
+
+  // method two by recursion
+  function steps2(n, row = 0, stair = ''){
+    if(n === row){
+        return;
+    }
+
+    if(n === stair.length){
+        console.log(stair)
+        return steps2(n, row + 1)
+    }
+
+    if(stair.length <= row){
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps2(n, row, stair);
+ }
+
+ steps1(3);
+ steps1(4);
+
+ steps2(5);
+ steps2(6);
+
+ // recursion
+ function printNumber(n, dec = 1){
+     if(n === 0){
+         return;
+     }
+     console.log("check n", n);
+     printNumber(n - dec);
+ }
+
+ printNumber(10);
